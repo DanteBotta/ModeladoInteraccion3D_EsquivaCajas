1e56c0eb12e970c867e610dc7f2e62249a82edfd
Damptermite4040 <dantegiovannibotta@gmail.com>
2026-05-07 10:18:11 -0300

    CajaMovement Hecho

---
+++ Assets/Scripts/CajaMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float VelocidadCaja = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -VelocidadCaja * Time.deltaTime, 0);
+        if (transform.position.y <= -1)
+        {
+            transform.Translate(0, 9, 0);
+        }
+    }
+}
